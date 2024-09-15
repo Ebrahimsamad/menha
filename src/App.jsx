cf927f5e842a07a37f05c40952582ba51e88aef5
Ebrahimsamad <ebrahim7asn@gmail.com>
2024-09-15 21:46:43 +0300

    FEAT:Add Layout , Navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import ResetPasswordPage from "./pages/ResetPasswordPage";
 
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./ui/ProtectedRoute";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
@@ -35,49 +36,51 @@ function App() {
       />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route
-            path="/login"
-            element={
-              <ProtectedRoute>
-                <LoginPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/signup"
-            element={
-              <ProtectedRoute>
-                <SignupPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/forget-password"
-            element={
-              <ProtectedRoute>
-                <ForgetPasswordPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/check-reset-password-token"
-            element={
-              <ProtectedRoute>
-                <CheckTokenPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/reset-password"
-            element={
-              <ProtectedRoute>
-                <ResetPasswordPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<PageNotFound />} />
+          <Route element={<Layout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route
+              path="/login"
+              element={
+                <ProtectedRoute>
+                  <LoginPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <ProtectedRoute>
+                  <SignupPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/forget-password"
+              element={
+                <ProtectedRoute>
+                  <ForgetPasswordPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/check-reset-password-token"
+              element={
+                <ProtectedRoute>
+                  <CheckTokenPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/reset-password"
+              element={
+                <ProtectedRoute>
+                  <ResetPasswordPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
